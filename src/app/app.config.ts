2bfe8e35f60b9a12e870193f9011e5c43da96758
swiftAndHandy <158059006+swiftAndHandy@users.noreply.github.com>
2026-04-16 09:44:09 +0200

    feat: APIInterceptor/ErrorHandler

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,23 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, isDevMode } from '@angular/core';
+import {ApplicationConfig, provideBrowserGlobalErrorListeners, isDevMode, ErrorHandler} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import {provideHttpClient, withInterceptors} from '@angular/common/http';
 import { TranslocoHttpLoader } from './core/i18n/transloco-loader';
 import { provideTransloco } from '@jsverse/transloco';
+import {apiInterceptor} from './core/http/api-interceptor';
+import {GlobalErrorHandler} from './core/error/error-handler';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(
+      withInterceptors([apiInterceptor])
+    ), {
+      provide: ErrorHandler,
+      useClass: GlobalErrorHandler
+    },
     provideTransloco({
       config: {
         availableLangs: ['de', 'en'],
