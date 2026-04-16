7ee5f65b052f08264fce93f362758b670bfaeea2
swiftAndHandy <158059006+swiftAndHandy@users.noreply.github.com>
2026-04-16 09:25:23 +0200

    feat: footer init

--- src/app/layout/footer/footer.ts
+++ src/app/layout/footer/footer.ts
@@ -2,19 +2,22 @@ import {Component, inject} from '@angular/core';
 import {RouterLink, RouterLinkActive} from "@angular/router";
 import {TranslocoDirective} from "@jsverse/transloco";
 import {LangService} from '../../core/i18n/lang.service';
+import {LinkedInIcon} from '../../shared/icons/linkedin';
 
 @Component({
   selector: 'app-footer',
   imports: [
     RouterLinkActive,
     TranslocoDirective,
-    RouterLink
+    RouterLink,
+    LinkedInIcon
   ],
   templateUrl: './footer.html',
   styleUrl: './footer.css',
 })
 export class Footer {
-  private langService = inject(LangService)
+  private langService = inject(LangService);
+  protected year = new Date().getFullYear();
 
   toPath(path: string) {
     return this.langService.routerLinkToPath(path);
