b5ca09c5789e50891796f132845d8d1531859aca
swiftAndHandy <158059006+swiftAndHandy@users.noreply.github.com>
2026-04-18 11:27:27 +0200

    a11y: improved aria-labels for traffic-light-bar by using transloco

--- src/app/shared/traffic-light-bar/traffic-light-bar.ts
+++ src/app/shared/traffic-light-bar/traffic-light-bar.ts
@@ -1,5 +1,5 @@
-import {Component, input, output} from '@angular/core';
-import {TranslocoDirective} from '@jsverse/transloco';
+import {afterNextRender, Component, computed, inject, input, output} from '@angular/core';
+import {TranslocoDirective, TranslocoService} from '@jsverse/transloco';
 
 @Component({
   selector: 'app-traffic-light-bar',
@@ -13,4 +13,12 @@ export class TrafficLightBar {
   close = output();
   fullscreen = output();
   isFullscreen = input(true);
+  target = input('');
+
+  translocoService = inject(TranslocoService);
+
+  protected resolvedTarget = computed(() =>
+    this.target() || this.translocoService.translate('action.targetMissing')
+  );
+
 }
