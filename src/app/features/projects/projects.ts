433c4bc4b89317cd86f14c90b686a715ee3dccb5
swiftAndHandy <158059006+swiftAndHandy@users.noreply.github.com>
2026-04-20 09:06:06 +0200

    feat: projects filter

--- src/app/features/projects/projects.ts
+++ src/app/features/projects/projects.ts
@@ -1,15 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, computed, signal} from '@angular/core';
 import {projects} from './projects.data';
 import {ProjectCard} from './components/project-card/project-card';
+import {TranslocoDirective} from '@jsverse/transloco';
 
 @Component({
   selector: 'app-projects',
   imports: [
-    ProjectCard
+    ProjectCard,
+    TranslocoDirective
   ],
   templateUrl: './projects.html',
   styleUrl: './projects.css',
 })
 export class Projects {
   protected readonly projects = projects;
+
+  protected readonly filterOptions: ('frontend' | 'backend' | 'both')[] = ['frontend', 'backend', 'both'];
+
+  protected readonly activeFilter =
+    signal<'frontend' | 'backend' | 'both'>('both');
+
+  protected readonly filteredProjects = computed(() => {
+    return this.activeFilter() === 'both'
+      ? projects
+      : projects.filter(project => project.category === this.activeFilter());
+  });
+
+
 }
