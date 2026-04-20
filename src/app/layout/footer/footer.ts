8c7d8ec5d3303700b5b4e89605e576daa8d0e320
swiftAndHandy <158059006+swiftAndHandy@users.noreply.github.com>
2026-04-20 11:19:56 +0200

    style: new header layout

--- src/app/layout/footer/footer.ts
+++ src/app/layout/footer/footer.ts
@@ -3,6 +3,9 @@ import {RouterLink, RouterLinkActive} from "@angular/router";
 import {TranslocoDirective} from "@jsverse/transloco";
 import {LangService} from '../../core/i18n/lang.service';
 import {LinkedInIcon} from '../../shared/icons/linkedin';
+import {NgOptimizedImage} from '@angular/common';
+import {ColorSchemeService} from '../../core/theming/color-scheme.service';
+import {Tooltip} from '../../shared/tooltip/tooltip';
 
 @Component({
   selector: 'app-footer',
@@ -10,14 +13,18 @@ import {LinkedInIcon} from '../../shared/icons/linkedin';
     RouterLinkActive,
     TranslocoDirective,
     RouterLink,
-    LinkedInIcon
+    LinkedInIcon,
+    NgOptimizedImage,
+    Tooltip
   ],
   templateUrl: './footer.html',
   styleUrl: './footer.css',
 })
 export class Footer {
   private langService = inject(LangService);
+  protected colorSchemeService = inject(ColorSchemeService);
   protected year = new Date().getFullYear();
+
 
   toPath(path: string) {
     return this.langService.routerLinkToPath(path);
